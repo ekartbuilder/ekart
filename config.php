f844830bcd39502e7a60ddcbdb3173d91d45766c
ekart <ekartbuilder@gmail.com>
2015-08-30 22:31:48 +0530

    ignoring config and cache

--- config.php
+++ config.php
@@ -6,23 +6,23 @@ define('HTTP_SERVER', 'http://www.ekartbuilder.com/');
 define('HTTPS_SERVER', 'http://www.ekartbuilder.com/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/ekartbuilder/catalog/');
-define('DIR_SYSTEM', '/var/www/html/ekartbuilder/system/');
-define('DIR_LANGUAGE', '/var/www/html/ekartbuilder/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/html/ekartbuilder/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/html/ekartbuilder/system/config/');
-define('DIR_IMAGE', '/var/www/html/ekartbuilder/image/');
-define('DIR_CACHE', '/var/www/html/ekartbuilder/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/ekartbuilder/system/download/');
-define('DIR_UPLOAD', '/var/www/html/ekartbuilder/system/upload/');
-define('DIR_MODIFICATION', '/var/www/html/ekartbuilder/system/modification/');
-define('DIR_LOGS', '/var/www/html/ekartbuilder/system/logs/');
+define('DIR_APPLICATION', 'D:/wamp/www/ekart/catalog/');
+define('DIR_SYSTEM', 'D:/wamp/www/ekart/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/ekart/catalog/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/ekart/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/wamp/www/ekart/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/ekart/image/');
+define('DIR_CACHE', 'D:/wamp/www/ekart/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/ekart/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/ekart/system/upload/');
+define('DIR_MODIFICATION', 'D:/wamp/www/ekart/system/modification/');
+define('DIR_LOGS', 'D:/wamp/www/ekart/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'ekart_db');
-define('DB_PASSWORD', 'Ak4j62K3EK73sM44');
+define('DB_USERNAME', 'atul');
+define('DB_PASSWORD', 'atul');
 define('DB_DATABASE', 'ekart');
 define('DB_PORT', '3306');
 define('DB_PREFIX', '');
