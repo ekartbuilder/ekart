62641714a8318c02402d4ee843971916542ee51d
root <root@www.taggedinfo.com>
2015-08-30 12:33:14 +0530

    base code import

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.ekartbuilder.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.ekartbuilder.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/ekartbuilder/catalog/');
+define('DIR_SYSTEM', '/var/www/html/ekartbuilder/system/');
+define('DIR_LANGUAGE', '/var/www/html/ekartbuilder/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/ekartbuilder/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/ekartbuilder/system/config/');
+define('DIR_IMAGE', '/var/www/html/ekartbuilder/image/');
+define('DIR_CACHE', '/var/www/html/ekartbuilder/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/ekartbuilder/system/download/');
+define('DIR_UPLOAD', '/var/www/html/ekartbuilder/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/ekartbuilder/system/modification/');
+define('DIR_LOGS', '/var/www/html/ekartbuilder/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ekart_db');
+define('DB_PASSWORD', 'Ak4j62K3EK73sM44');
+define('DB_DATABASE', 'ekart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
