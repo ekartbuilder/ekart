5111be24912198729760338ce5f61ba832ae5527
ekart <ekartbuilder@gmail.com>
2015-09-05 16:02:09 +0530

    adding local config files

---
+++ admin/config_local.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ekart/admin/');
+define('HTTP_CATALOG', 'http://localhost/ekart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ekart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/ekart/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/wamp/www/ekart/admin/');
+define('DIR_SYSTEM', 'D:/wamp/www/ekart/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/ekart/admin/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/ekart/admin/view/template/');
+define('DIR_CONFIG', 'D:/wamp/www/ekart/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/ekart/image/');
+define('DIR_CACHE', 'D:/wamp/www/ekart/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/ekart/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/ekart/system/upload/');
+define('DIR_LOGS', 'D:/wamp/www/ekart/system/logs/');
+define('DIR_MODIFICATION', 'D:/wamp/www/ekart/system/modification/');
+define('DIR_CATALOG', 'D:/wamp/www/ekart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'atul');
+define('DB_PASSWORD', 'atul');
+define('DB_DATABASE', 'ekart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config_local.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ekart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ekart/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/wamp/www/ekart/catalog/');
+define('DIR_SYSTEM', 'D:/wamp/www/ekart/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/ekart/catalog/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/ekart/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/wamp/www/ekart/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/ekart/image/');
+define('DIR_CACHE', 'D:/wamp/www/ekart/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/ekart/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/ekart/system/upload/');
+define('DIR_MODIFICATION', 'D:/wamp/www/ekart/system/modification/');
+define('DIR_LOGS', 'D:/wamp/www/ekart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'atul');
+define('DB_PASSWORD', 'atul');
+define('DB_DATABASE', 'ekart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
