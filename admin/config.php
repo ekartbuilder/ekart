ccfbfcc70c05ba0db37bbb5d7dd3584e0e123d11
ekart <ekartbuilder@gmail.com>
2015-09-11 15:58:45 +0530

    added remote configs

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.ekartbuilder.com/admin/');
+define('HTTP_CATALOG', 'http://www.ekartbuilder.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.ekartbuilder.com/admin/');
+define('HTTPS_CATALOG', 'http://www.ekartbuilder.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/ekartbuilder/admin/');
+define('DIR_SYSTEM', '/var/www/html/ekartbuilder/system/');
+define('DIR_LANGUAGE', '/var/www/html/ekartbuilder/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/ekartbuilder/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/ekartbuilder/system/config/');
+define('DIR_IMAGE', '/var/www/html/ekartbuilder/image/');
+define('DIR_CACHE', '/var/www/html/ekartbuilder/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/ekartbuilder/system/download/');
+define('DIR_UPLOAD', '/var/www/html/ekartbuilder/system/upload/');
+define('DIR_LOGS', '/var/www/html/ekartbuilder/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/ekartbuilder/system/modification/');
+define('DIR_CATALOG', '/var/www/html/ekartbuilder/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ekart_db');
+define('DB_PASSWORD', 'Ak4j62K3EK73sM44');
+define('DB_DATABASE', 'ekart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
