385f3933285f729973244e039596e3232c7614f8
ekart <ekartbuilder@gmail.com>
2015-10-11 00:37:01 +0530

    defined constant support for php in twig

--- config_local.php
+++ config_local.php
@@ -29,6 +29,10 @@ define('PAGECACHE_DEF_LANG', 'en');
 define('PAGECACHE_DEF_CUR', 'inr');
 define('PAGECACHE_DEF_DEVICE', 'W');
 
+// TEMPLATE
+define('TWIG_PHP', true);
+//define('TWIG_CACHE', DIR_CACHE . 'twig');
+
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
