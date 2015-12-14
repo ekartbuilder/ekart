87d8b0a8ec4de8cf48c644deac23de8c856ec204
ekart <ekartbuilder@gmail.com>
2015-12-14 21:55:47 +0530

    deploy actions added

--- deploy.php
+++ deploy.php
@@ -1,4 +1,18 @@
-<?php
-$cmd = "git status";
-echo `$cmd`;
+<a href="deploy.php?action=status">Status</a> | <a href="deploy.php?action=pull">Pull</a>
+<?php 
+if(!empty($_GET['action'])) {
+	echo "<pre>";
+	switch ($_GET['action']) {
+		case 'status':
+			$cmd = "git status";			
+			break;
+		case 'pull':
+			$cmd = "git pull --rebase";			
+			break;
+		default:
+			
+			break;
+	}
+	echo `$cmd`;
+}
 ?>
