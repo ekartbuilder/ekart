3e305d4e9de4d9f441874de51dd2cf91b52ff9a5
ekart <ekartbuilder@gmail.com>
2016-04-23 19:35:52 +0530

    return id added for module

--- admin/model/extension/module.php
+++ admin/model/extension/module.php
@@ -2,6 +2,8 @@
 class ModelExtensionModule extends Model {
 	public function addModule($code, $data) {
 		$this->db->query("INSERT INTO `" . DB_PREFIX . "module` SET `name` = '" . $this->db->escape($data['name']) . "', `code` = '" . $this->db->escape($code) . "', `setting` = '" . $this->db->escape(serialize($data)) . "'");
+		$module_id = $this->db->getLastId();
+		return $module_id;
 	}
 	
 	public function editModule($module_id, $data) {
